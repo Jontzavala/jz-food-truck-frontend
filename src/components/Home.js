fb19fe59e263087db2c570cd19c6d707ff00f515
Jonathan Zavala <jontzavala@gmail.com>
2022-05-01 12:54:57 -0500

    created the home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+
+    render() {
+        return (
+            <h1>Welcome to JZ's Food Truck</h1>
+        )
+    }
+}
+
+export default Home
