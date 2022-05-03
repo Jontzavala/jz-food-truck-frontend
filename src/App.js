ba610a56874530c19b821fdeaf8f2c8ca1cdf076
Jonathan Zavala <jontzavala@gmail.com>
2022-05-03 10:17:44 -0500

    added sass and started syling categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './components/Home'
 import React from 'react';
+import './App.scss'
 
 function App() {
   return (
--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {Container, Card, Row, Col} from 'react-bootstrap'
 
 const CategoriesList = (props) => {
     let categories = props.categories
 
     return (
         <div>
-            {categories.map(category => <div>{category.name}</div>)}
+            <Container fluid>
+                <Row xs={1} md={2} lg={4} xl={6} classname='g-8'>
+                    {categories.map(category => 
+                        <Col key={category.id} className='d-flex'>
+                            <Card style={{ width: '15rem' }} className='text-center p-3 m-2 mx-auto shadow'>
+                                {category.name}
+                            </Card>
+                        </Col>
+                    )}
+                </Row>
+            </Container>
         </div>
     )
 }
