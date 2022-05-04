8ae1bbdcc545860b2cdf8d76378c87d1f59e872c
Jonathan Zavala <jontzavala@gmail.com>
2022-05-04 13:09:55 -0500

    centered the main header

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to JZ's Food Truck</h1>
+                <h1 className='main-header'>Welcome to JZ's Food Truck</h1>
                 <CategoriesContainer/>
             </div>
         )
